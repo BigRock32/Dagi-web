adee90c02925df37548678c81c6f7182b6ee03f5
BigRock32 <vasya.samarin.00@mail.ru>
2022-12-09 13:55:42 +0800

    Add block scroll

--- js/index.js
+++ js/index.js
@@ -93,3 +93,27 @@ let questionHeaderClickHandler = function(e){
 }
 questOpen()
 //Конец анимации аккордиона
+
+
+//Прокрутка страницы
+const menuLinks = document.querySelectorAll('header__link,footer__link,[data-goto]')
+if(menuLinks.length > 0){
+    menuLinks.forEach(menuLink => {
+        menuLink.addEventListener('click', onMenuLinkClick)
+    })
+
+    function onMenuLinkClick(e){
+        const menuLink = e.target;
+        if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+            const gotoBlock = document.querySelector(menuLink.dataset.goto);
+            const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight
+
+            window.scrollTo({
+                top: gotoBlockValue,
+                behavior: 'smooth'
+            });
+            e.preventDefault();
+        }
+    }
+}
+//Конец кода прокрутки страницы
