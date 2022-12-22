2ccb6e402d0e924792fab2d0fef884ee22803491
BigRock32 <vasya.samarin.00@mail.ru>
2022-12-22 10:31:05 +0800

    Co-authored-by: Vanya Samarin <mirageN1349@users.noreply.github.com>

--- js/index.js
+++ js/index.js
@@ -2,22 +2,22 @@
 
 //Анимация шапки
 const header = document.querySelector('.header');
-    
-    window.addEventListener('scroll', () => {
+
+window.addEventListener('scroll', () => {
     let scrollTop = window.pageYOffset;
 
     if (scrollTop >= 10) {
         header.classList.add('_fixed');
-    }else{
+    } else {
         header.classList.remove('_fixed');
     }
-    });
+});
 
 // const mediaQuery = window.matchMedia('(min-width: 768px)')
 
 // function handleTabletChange(e) {
 //     if (e.matches) {
-        
+
 //         document.addEventListener('DOMContentLoaded', function(){
 //             console.log('Ready');
 //         })
@@ -32,7 +32,7 @@ const header = document.querySelector('.header');
 //     if (window.innerWidth < 769) {
 //         window.addEventListener('scroll', () => {
 //             let scrollTop = window.pageYOffset;
-        
+
 //             if (scrollTop >= 10) {
 //                 header.classList.remove('_fixed');
 //             }else{
@@ -66,30 +66,30 @@ let arrow = document.querySelector('.questions__question')
 let questBodys = document.querySelectorAll('.questions__question-head')
 let quest = document.querySelectorAll('.questions__question')
 
-function initQuestion(){
+function initQuestion() {
     let firstQuestionBodyHeight = document.querySelector('.questions__question .questions__question-body > *').clientHeight
     questBody.style.maxHeight = firstQuestionBodyHeight + 'px'
 }
 initQuestion()
 
-function questOpen(){
+function questOpen() {
 
-let questionHeaderClickHandler = function(e){
-    quest.forEach(function(question){
-        question.querySelector('.questions__question-body').style.maxHeight = '0px'
-        question.classList.remove('rotate')
-    })
+    let questionHeaderClickHandler = function (e) {
+        quest.forEach(function (question) {
+            question.querySelector('.questions__question-body').style.maxHeight = '0px'
+            question.classList.remove('rotate')
+        })
 
-    let questionSection = e.target.closest('.questions__question')
+        let questionSection = e.target.closest('.questions__question')
 
-    let insideElHeight = questionSection.querySelector('.questions__question-body > *').clientHeight
+        let insideElHeight = questionSection.querySelector('.questions__question-body > *').clientHeight
 
-    questionSection.querySelector('.questions__question-body').style.maxHeight = insideElHeight + 'px'
-    questionSection.classList.add('rotate')
-}
+        questionSection.querySelector('.questions__question-body').style.maxHeight = insideElHeight + 'px'
+        questionSection.classList.add('rotate')
+    }
     arrow.classList.add('rotate')
 
-    questBodys.forEach(function(question){
+    questBodys.forEach(function (question) {
         question.addEventListener('click', questionHeaderClickHandler)
     })
 }
@@ -99,18 +99,18 @@ questOpen()
 
 //Прокрутка страницы
 const menuLinks = document.querySelectorAll('header__link,footer__link,[data-goto]')
-if(menuLinks.length > 0){
+if (menuLinks.length > 0) {
     menuLinks.forEach(menuLink => {
         menuLink.addEventListener('click', onMenuLinkClick)
     })
 
-    function onMenuLinkClick(e){
+    function onMenuLinkClick(e) {
         const menuLink = e.target;
-        if(menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)){
+        if (menuLink.dataset.goto && document.querySelector(menuLink.dataset.goto)) {
             const gotoBlock = document.querySelector(menuLink.dataset.goto);
             const gotoBlockValue = gotoBlock.getBoundingClientRect().top + pageYOffset - document.querySelector('header').offsetHeight
 
-            if(burger.classList.contains('_active')){
+            if (burger.classList.contains('_active')) {
                 document.body.classList.remove('_lock')
                 burger.classList.remove('_active')
                 burgerMenu.classList.remove('_active')
@@ -130,8 +130,8 @@ if(menuLinks.length > 0){
 //Анимация бургера
 const burger = document.querySelector('.burger')
 const burgerMenu = document.querySelector('.burger-menu')
-if(burger){
-    burger.addEventListener('click', function(e){
+if (burger) {
+    burger.addEventListener('click', function (e) {
         document.body.classList.toggle('_lock')
         burger.classList.toggle('_active')
         burgerMenu.classList.toggle('_active')
@@ -141,59 +141,88 @@ if(burger){
 
 
 //Модальное окно
-const btns = document.querySelectorAll('.services__item')
+const btns = document.querySelectorAll('.popup-link')
 const modal = document.querySelector('.modal')
-const cross = document.querySelector('.modal__close-icon')
-const closeBg = document.querySelector('.modal__close-bg')
+const modals = document.querySelectorAll('.modal')
+const modalOverlay = document.querySelector('.modal-overlay')
+const cross = document.querySelectorAll('.modal__close-icon')
 const formTitle = document.querySelector('.form__change-title')
 
-if(btns.length > 0){
+
+if (btns.length > 0) {
     btns.forEach(btn => {
-        btn.addEventListener('click', function(e){
-            modal.classList.add('_open')
-            document.body.classList.add('_lock')
-            e.preventDefault();
-        })
+        let path = btn.dataset.path;
+
+        switch (path) {
+            case 'one':
+                btn.addEventListener('click', btnHandler);
+                break;
+            case 'two':
+                btn.addEventListener('click', btnHandler2);
+                break;
+        }
     })
 }
 
-btns.forEach((btn) => btn.addEventListener('click', btnHandler));
+cross.forEach((cros) => {
+    cros.addEventListener('click', closeModal);
+})
 
-cross.addEventListener('click', closeModal);
 
-closeBg.addEventListener('click', (e)=>{
-    if(e.target == closeBg){
+modalOverlay.addEventListener('click', (e) => {
+    if (e.target == modalOverlay) {
         closeModal()
     }
 })
 
-function closeModal(){
-    modal.classList.remove('_open')
+
+function openMpdal(path) {
+    document.querySelector(`[data-target="${path}"]`).classList.add('_open');
+    modalOverlay.classList.add('_open');
+    document.body.classList.add('_lock');
+}
+
+function closeModal() {
+    modals.forEach((modal) => {
+        modal.classList.remove('_open')
+    })
+    modalOverlay.classList.remove('_open')
     document.body.classList.remove('_lock')
 }
 
-function btnHandler(e){
-    const parent = e.target.closest('.services__item');
-    const title = parent.querySelector('.services__hidden-title');
+function btnHandler(e) {
+    e.preventDefault()
+    const path = e.currentTarget.getAttribute('data-path');
+    openMpdal(path)
+    const parent = e.target.closest('.popup-link');
+    const title = parent.querySelector('.popup-title');
     const titleText = title.innerText;
     formTitle.innerText = titleText;
+}
+
+function btnHandler2(e) {
+    e.preventDefault()
+    const path = e.currentTarget.getAttribute('data-path');
+    const currentModal = document.querySelector(`[data-target="${path}"]`)
+    currentModal.querySelector('.form__change-title').innerText = e.target.innerText
+    openMpdal(path)
 }
 //Конец кода модального окна
 
 
 //Форма обратной связи
-document.addEventListener('DOMContentLoaded', function(){
+document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('form')
     form.addEventListener('submit', formSend)
 
-    async function formSend(e){
+    async function formSend(e) {
         e.preventDefault()
 
         let error = formValidate(form)
 
         let formData = new FormData(form)
 
-        if(error === 0){
+        if (error === 0) {
             form.classList.add('_sending')
             // let response = await fetch('sendmail.php',{
             //     method: 'POST',
@@ -209,26 +238,26 @@ document.addEventListener('DOMContentLoaded', function(){
             //     alert('Ошибка')
             //     form.classList.remove('_sending')
             // }
-        }else{
+        } else {
             alert('Заполните обязательные поля')
         }
     }
 
-    function formValidate(e){
+    function formValidate(e) {
         let error = 0
         let formReq = document.querySelectorAll('._req')
 
-        for(let index = 0; index < formReq.length; index++){
+        for (let index = 0; index < formReq.length; index++) {
             const input = formReq[index]
             formRemoveError(input)
 
-            if(input.classList.contains('_email')){
-                if(emailTest(input)){
+            if (input.classList.contains('_email')) {
+                if (emailTest(input)) {
                     formAddError(input)
                     error++
                 }
-            }else{
-                if(input.value === ''){
+            } else {
+                if (input.value === '') {
                     formAddError(input)
                     error++
                 }
@@ -237,17 +266,17 @@ document.addEventListener('DOMContentLoaded', function(){
         return error
     }
 
-    function formAddError(input){
+    function formAddError(input) {
         input.parentElement.classList.add('_error')
         input.classList.add('_error')
     }
 
-    function formRemoveError(input){
+    function formRemoveError(input) {
         input.parentElement.classList.remove('_error')
         input.classList.remove('_error')
     }
 
-    function emailTest(input){
+    function emailTest(input) {
         return !/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/.test(input.value);
     }
 })
